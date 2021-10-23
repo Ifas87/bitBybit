98f884543814eac60f09129d2d08cccd21bbb915
Safi <safi.sharieff@gmail.com>
2021-10-23 16:18:40 +0400

    23-10-2021 Changes -Front end design of the index HTML is completed -Project environment has been transferred

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('./external_files/html/index.html')
 
 if __name__=='__main__':
     app.run()
