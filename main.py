d3feeae5e758836e292ea6edef8e1c226349dbf9
Safi <safi.sharieff@gmail.com>
2021-10-25 00:22:48 +0400

    24-10-2021 Changes - Updates responsiveness values to the default 100% screen zoom - Added Upload page elements - added dropzone from flask

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from flask import *
+from flask_dropzone import Dropzone
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config.update(
+    UPLOADED_PATH= os.path.join(basedir,'uploads'),
+    DROPZONE_MAX_FILE_SIZE = 1024,
+    DROPZONE_TIMEOUT = 5*60*1000
+)
+
+dropzone = Dropzone(app)
+
+@app.route('/upload',methods=['POST','GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join(app.config['UPLOADED_PATH'],f.filename))
+    return render_template('upload.html', template_folder='templates')
 
 @app.route('/')
 def hello():
