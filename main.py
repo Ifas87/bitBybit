a4c17d8196d1ed44fe73cf89fda445a5ef13f8d0
Safi <safi.sharieff@gmail.com>
2021-10-26 00:17:15 +0400

    26-10-2021 Changes
    -Added uploads.html
    -added some incomplete backend logic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import *
 from flask_dropzone import Dropzone
 import os
+import time
+from datetime import datetime
+import tarfile
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -20,6 +23,23 @@ def upload():
         f = request.files.get('file')
         f.save(os.path.join(app.config['UPLOADED_PATH'],f.filename))
     return render_template('upload.html', template_folder='templates')
+
+@app.route('/uploader',methods=['POST','GET'])
+def download_manager():
+    my_files = request.files
+    current_time = datetime.now()
+    temp_filename = "file_"+current_time.strftime("%m_%d_%Y_%H_%M_%S_%f")+"_package.tar.gz"
+
+    tar = tarfile.open("uploads/"+temp_filename, mode="w:gz")
+    for item in my_files:
+        uploaded_file = my_files.get(item)
+        tarlocation = os.path.join(app.config['UPLOADED_PATH']+"/temp", item.filename)
+        item.save(tarlocation)
+        tar.add(tarlocation)
+    
+    tar.close()
+    return redirect(url_for('hello'))
+
 
 @app.route('/')
 def hello():
